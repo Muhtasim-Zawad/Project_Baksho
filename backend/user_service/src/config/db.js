b46d4705f7d05a60822e8b15986e27a83d3a355c
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2025-08-23 20:11:21 +0600

    updated docker compose to user_service

--- backend/user_service/src/config/db.js
+++ backend/user_service/src/config/db.js
@@ -1,15 +1,14 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const connectDB = async () => {
-    try {
-        const uri = process.env.MONGO_URI;
-        const conn = await mongoose.connect(uri);
-        console.log('Db connected!!');
-    } catch (error) {
-        console.log(`error: ${error.message}`)
-    }
-}
-
+  try {
+    const uri = process.env.MONGO_URI;
+    const conn = await mongoose.connect(uri);
+    console.log("Db connected!!");
+  } catch (error) {
+    console.log(`error: ${error.message}`);
+  }
+};
--- backend/user_service/src/server.js
+++ backend/user_service/src/server.js
@@ -1,38 +1,40 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
-import { connectDB } from './config/db.js';
+import { connectDB } from "./config/db.js";
 
-import authRoutes from './routes/authRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT ||  8000;
-
+const PORT = process.env.PORT || 8000;
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: ['http://localhost:5173','http://localhost:5174', 'http://localhost:3000'], 
-  credentials: true
-}));
-
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+  }),
+);
 
 //db connection
 connectDB();
 
 //auth route
-app.use('/api/users/auth', authRoutes)
+app.use("/api/users/auth", authRoutes);
 //user route
-app.use('/api/users/', userRoutes)
+app.use("/api/users/", userRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Baksho is Running!!!');
+app.get("/", (req, res) => {
+  res.send("Baksho is Running!!!");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
