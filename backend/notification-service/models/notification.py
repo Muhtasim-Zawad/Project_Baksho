8045e90013a16123d063fe6fa306f5179de47a73
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2025-08-27 01:44:09 +0600

    notification sytem schema

---
+++ backend/notification-service/models/notification.py
@@ -0,0 +1,11 @@
+# models/campaign.py
+from typing import List, Optional
+from sqlmodel import Field, Relationship, SQLModel
+import datetime
+
+class Notification(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: str
+    notification : str
+    seen : bool = Field(default=False)
+    created_at: datetime.datetime = Field(default_factory=datetime.datetime.utcnow)
